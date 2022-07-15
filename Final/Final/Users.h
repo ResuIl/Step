ee4802217f443a31ab02f16ba18c32bda6126304
Resul <71267982+ResuIl@users.noreply.github.com>
2022-07-15 10:54:50 +0400

    Optimization

--- Final/Final/Users.h
+++ Final/Final/Users.h
@@ -6,7 +6,23 @@ class Users {
     int _score;
 public:
 
-    Users(string username, string password) : _username(username), _password(password) {};
+    Users(string username, string password) : _username(username), _password(password), _score() {};
+
+    void signUp() {
+        ofstream fout("Users.txt", ios_base::app);
+
+        if (!fout)
+            throw new exception("File can not created");
+
+        if (!fout.is_open()) {
+            fout.close();
+            throw new exception("File can not opened");
+        }
+
+        fout << _username << ":" << _password << ":" << _score << endl;
+
+        fout.close();
+    }
 
     void setUserScore(int score) {
         _score = score;
