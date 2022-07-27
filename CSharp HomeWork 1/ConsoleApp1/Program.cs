1a720541b734bf088bfd6f511c9019184881ffb6
Rəsul <71267982+ResuIl@users.noreply.github.com>
2022-07-27 11:05:11 +0400

    Add files via upload

---
+++ CSharp HomeWork 1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    enum MathOperator
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+    };
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int Add(int a, int b) { return a + b; }
+
+            int Subtract(int a, int b) { return a - b; }
+
+            int Multiply(int a, int b) { return a * b; }
+
+            int Divide(int a, int b) { return a / b; }
+
+            Console.Write("Enter A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter B: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"1) {A} + {B}\n2) {A} - {B}\n3) {A} * {B}\n4) {A} / {B}\n");
+            Console.Write("Make Choice: ");
+            int op = Convert.ToInt32(Console.ReadLine());
+
+            switch(op-1)
+            {
+                case 0:
+                    Console.Write(Add(A, B));
+                    break;
+                case 1:
+                    Console.Write(Subtract(A, B));
+                    break;
+                case 2:
+                    Console.Write(Multiply(A, B));
+                    break;
+                case 3:
+                    Console.Write(Divide(A, B));
+                    break;
+            }
+
+        }
+    }
+}
