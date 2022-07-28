44d35d3ed0355b8e79755f2dd9037182294f2015
Rəsul <71267982+ResuIl@users.noreply.github.com>
2022-07-29 00:34:27 +0400

    Update Program.cs

--- CSharp HomeWork 1/ConsoleApp1/Program.cs
+++ CSharp HomeWork 1/ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp1
 {
@@ -20,30 +20,43 @@ namespace ConsoleApp1
 
             int Multiply(int a, int b) { return a * b; }
 
-            int Divide(int a, int b) { return a / b; }
+            int Divide(int a, int b) {
+                if (a == 0 || b == 0)
+                {
+                    Console.Write("ERROR\n");
+                    return 0;
+                }
+                return a / b; 
+            }
 
-            Console.Write("Enter A: ");
-            int A = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter B: ");
-            int B = Convert.ToInt32(Console.ReadLine());
-            Console.Write($"1) {A} + {B}\n2) {A} - {B}\n3) {A} * {B}\n4) {A} / {B}\n");
-            Console.Write("Make Choice: ");
-            int op = Convert.ToInt32(Console.ReadLine());
-
-            switch(op-1)
+            while (true)
             {
-                case 0:
-                    Console.Write(Add(A, B));
-                    break;
-                case 1:
-                    Console.Write(Subtract(A, B));
-                    break;
-                case 2:
-                    Console.Write(Multiply(A, B));
-                    break;
-                case 3:
-                    Console.Write(Divide(A, B));
-                    break;
+                Console.Write("Enter A: ");
+                int A = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter B: ");
+                int B = Convert.ToInt32(Console.ReadLine());
+                Console.Write($"1) {A} + {B}\n2) {A} - {B}\n3) {A} * {B}\n4) {A} / {B}\n5) Exit\n");
+                Console.Write("Make Choice: ");
+                int op = Convert.ToInt32(Console.ReadLine());
+
+                switch (op - 1)
+                {
+                    case 0:
+                        Console.Write(Add(A, B) + Environment.NewLine);
+                        break;
+                    case 1:
+                        Console.Write(Subtract(A, B) + Environment.NewLine);
+                        break;
+                    case 2:
+                        Console.Write(Multiply(A, B) + Environment.NewLine);
+                        break;
+                    case 3:
+                        Console.Write(Divide(A, B) + Environment.NewLine);
+                        break;
+                    case 4:
+                        System.Environment.Exit(1);
+                        break;
+                }
             }
 
         }
